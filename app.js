64a62b249ff0a155cc8d86acc8fa593e3fe1c93f
RonaldoBelem <ronaldonunes8@gmail.com>
2024-09-30 22:23:26 -0300

    ajuste do popup

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const body = document.querySelector("body");
 const div = document.createElement("div");
 
+/* coloquei todas as tags html dentro do documento javascript apenas para 
+mostra as manipulção do javascript  */
 body.appendChild(div);
 div.innerHTML = `
 <div class="box">
@@ -19,7 +21,7 @@ div.innerHTML = `
   <div id="popup" >  
     <div class="popup_resultado" id="contagem" >Aguarde a Contagem </div>  
     <div class="popup_resultado" id="resultado" ></div>  
-    <div class="popup_resultado " id="res" ></div>  
+    <div class="popup_resultado " id="novoSorteio" >Novo Sorteio</div>  
   </div>      
 </div>       
 `;
@@ -29,33 +31,36 @@ const input = document.getElementById("numero");
 const btn = document.getElementById("btn");
 const numerosSorteados = document.getElementById("numerosSorteados");
 const resultado = document.getElementById("resultado");
-const res = document.getElementById("res");
 const contagem = document.getElementById("contagem");
 const popup = document.getElementById("popup");
+const novoSorteio = document.getElementById("novoSorteio");
 
 
 
-
+/* botão que ativa a função para fazer o sorteio
+ e tambem abre uma contagem regessiva antes de mostrar o resultado */ 
 btn.addEventListener("click", () => {
   let cont = 3
   popup.style.display="flex"
+  /* função que abre a contagem regressiva */
   function contagemRegressiva() {
     if (cont >= 0) {
+      contagem.innerHTML="Aguarde a Contagem"
       resultado.style.color="red"
       resultado.innerHTML=cont
        cont--   
     }
     else{
       contagem.innerHTML="Fechar"
-      res.innerHTML=`Resultado`
-      res.style.margin="0 40px"
       resultado.style.color="white"
-      resultado.innerHTML=`${numeros.join(",  ")}`;
+      resultado.innerHTML= ` ${numeros.join(", ")}`;
+      resultado.style.textAlign="center"
       clearInterval(intervalo)
     }
   }
+
   const intervalo = setInterval( contagemRegressiva, 1000)
-  
+
   const quantidadeTotalNumeros = parseInt(quantidadeTotal.value);
   const quantidade = parseInt(input.value);
 
@@ -65,10 +70,47 @@ btn.addEventListener("click", () => {
       numeros.push(Math.floor(Math.random() * quantidadeTotalNumeros) + 1); 
     }
     
-    numerosSorteados.textContent = `Numeros Sorteados: ${numeros.join(",  ")}`;
+    numerosSorteados.textContent = `Numeros Sorteados: ${numeros.join(", ")}`;
     
 });
 
+/* evento que FECHAR o popup do numero sorteado */
 contagem.addEventListener("click", ()=>{
   popup.style.display="none"
 })
+
+
+novoSorteio.addEventListener("click", () => {
+  let cont = 3
+  popup.style.display="flex"
+  /* função que abre a contagem regressiva */
+  function contagemRegressiva() {
+    if (cont >= 0) {
+      contagem.innerHTML="Aguarde a Contagem"
+      resultado.style.color="red"
+      resultado.innerHTML=cont
+       cont--   
+    }
+    else{
+      contagem.innerHTML="Fechar"
+      resultado.style.color="white"
+      resultado.innerHTML= ` ${numeros.join(", ")}`;
+      resultado.style.textAlign="center"
+      clearInterval(intervalo)
+    }
+  }
+
+  const intervalo = setInterval( contagemRegressiva, 1000)
+
+  const quantidadeTotalNumeros = parseInt(quantidadeTotal.value);
+  const quantidade = parseInt(input.value);
+
+
+  const numeros = [];   
+    for (let i = 0; i < quantidade; i++) {
+      numeros.push(Math.floor(Math.random() * quantidadeTotalNumeros) + 1); 
+    }
+    
+    numerosSorteados.textContent = `Numeros Sorteados: ${numeros.join(", ")}`;
+    
+});
